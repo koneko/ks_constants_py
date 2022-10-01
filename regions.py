d5252d31037fd90a66fa80385ab51f1564734b8e
Luminous <seokhohong02@gmail.com>
2022-10-01 08:51:32 -0700

    finished regions

--- regions.py
+++ regions.py
@@ -1,34 +1,16 @@
-class region:
-    """ database """
-    _region_dict = {}
+from enum import Enum
 
-    """ contstructor call """
+class Region(Enum):
+    North_America = ({"English": 'NA', "Korean": '북미'}),
+    Europe = ({"English": 'EU', "Korean": '유럽'}),
+    Korea = ({"English": 'KR', "Korean": '한국'}),
+    China = ({"English": 'CN', "Korean": '중국'})
 
-    def __init__(self):
-        self._region_dict['en'] = self._add_key('Kerrigan Survival 2', {'na': 'NA', 'eu': 'EU', 'kr': 'KR', 'cn': 'CN'})
-        self._region_dict['kr'] = self._add_key('케리건 살아남기 2', {'na': '북미', 'eu': '유럽', 'kr': '한국', 'cn': '중국'})
+    def __init__(self, codes):
+        self._codes = codes
 
-    """ adding information to database """
+    def get_english_code(self):
+        return self._codes["English"]
 
-    def _add_key(self, in_title, in_region_display_name):
-        output = {
-            'title': in_title,
-            'region_display_name': in_region_display_name,
-        }
-        return output
-
-    """ Remove spaces and lowercase everything from str entry """
-
-    def _remove_noise(self, str: str):
-        str = str.replace(' ', '').lower()
-        return str
-
-    """ Returns KS2 title"""
-
-    def get_region_title(self, lang: str) -> str:
-        return self._region_dict[self._remove_noise(lang)]['title']
-
-    """ Returns Region Display Name"""
-
-    def get_region_name(self, lang: str, region: str) -> str:
-        return self._region_dict[self._remove_noise(lang)]['region_display_name'][self._remove_noise(region)]
+    def get_korean_code(self):
+        return self._codes["Korean"]
