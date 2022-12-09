b39821bd08586c125cb9ca32d02bd6b727a62270
anon3068993 <91652465+anon3068993@users.noreply.github.com>
2022-12-08 22:35:32 -0500

    increase version number (#7)
    Co-authored-by: Templar <>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='KS_Constants',
-    version='1.2.14',
+    version='1.2.17',
     license='MIT',
     author="Steven Su",
     author_email='ks2devteam@gmail.com',
